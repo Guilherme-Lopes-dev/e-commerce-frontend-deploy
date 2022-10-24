cc57089fdd510e094197acbea559d0f8c6796f47
Guilherme Lopes <guilherme.cpmg@hotmail.com>
2022-10-24 18:45:44 -0300

    Correcao de url

--- src/features/productsApi.js
+++ src/features/productsApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
 
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => "products",
--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -11,7 +11,7 @@ export const productsFetch = createAsyncThunk(
     async () => {
       try {
         const response = await axios.get(
-          process.env.REACT_APP_BASE_URL
+          process.env.REACT_APP_API_URL
         );
         return response?.data;
       } catch (error) {
